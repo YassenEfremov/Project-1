3df2a24cc4d407add5ed283c72026c15b0b758dc
Yassen <48584741+YassenEfremov@users.noreply.github.com>
2020-03-23 18:55:51 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='module1',
+      version='1.0.0',
+      description='My first module package',
+      url='https://github.com/YassenEfremov/Project-1',
+      author='Yassen Efremov',
+      author_email='yassen.efremov@gmail.com',
+      license='MIT',
+      packages=['module'],
+      zip_safe=False)
